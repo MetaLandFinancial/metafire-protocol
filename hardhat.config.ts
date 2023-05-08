118a479535c755526513130fb4fc88f725d16417
0xsolid22 <smart.solidity@gmail.com>
2023-05-08 13:19:30 -0400

    fix: add etherscan verification support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,54 +1,55 @@
-import * as dotenv from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
 // import '@nomiclabs/hardhat-waffle';
-require("hardhat-contract-sizer");
+require('hardhat-contract-sizer');
 
-import "./tasks/deploys/deploy";
-
+import './tasks/deploys/deploy';
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.4",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
 
   networks: {
     hardhat: {
       chainId: 1337,
-      gas: "auto",
-      gasPrice: "auto",
-      allowUnlimitedContractSize: true
+      gas: 'auto',
+      gasPrice: 'auto',
+      allowUnlimitedContractSize: true,
     },
     goerli: {
-      url: process.env.GOERLI_URL || "",
+      url: process.env.GOERLI_URL || '',
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
       chainId: 5,
-      gas: "auto",
+      gas: 'auto',
       allowUnlimitedContractSize: true,
     },
     sepolia: {
-      url: process.env.SEPOLIA_URL || "",
+      url: process.env.SEPOLIA_URL || '',
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111,
       // gas: "auto",
       allowUnlimitedContractSize: true,
-      gas: "auto",
-      gasPrice: "auto",
+      gas: 'auto',
+      gasPrice: 'auto',
     },
-    localhost:{
-      
-    }
-  }
+    localhost: {},
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
